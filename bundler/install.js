1cf72b43555809bbc0079f19ca7f0d0f7a584c2b
Sebastian Wilzbach <seb@wilzba.ch>
2015-01-30 08:15:42 +0100

    use the programmatic npm interface to install a package

--- bundler/install.js
+++ bundler/install.js
@@ -1,18 +1,28 @@
-var exec = require('child_process').exec;
 var q = require("bluebird");
+var npmi = require('npmi');
 
 module.exports = function(opts) {
 
-  var pkgVersion = opts.pkg.name + '@' + opts.pkg.version;
   var env = opts.env;
   var log = env.log;
 
+  var options = {
+    name: opts.pkg.name, // your module name
+    version: opts.pkg.version, // expected version [default: 'latest']
+    path: opts.env.dirPath, // installation path [default: '.']
+    forceInstall: false, // force install if set to true (even if already installed, it will do a reinstall) [default: false]
+    npmLoad: { // npm.load(options, callback): this is the "options" given to npm.load()
+      loglevel: 'silent' // [default: {loglevel: 'silent'}]
+    }
+  };
   return new q.Promise(function(resolve, reject) {
-    env.exec("npm install " + pkgVersion, function(err, stdout, stderr) {
+    npmi(options, function(err, result) {
       if (err) {
-        return reject(err);
+        if (err.code === npmi.LOAD_ERR) reject('npm load error');
+        else if (err.code === npmi.INSTALL_ERR) reject('npm install error');
+        return reject(err.message);
       }
-      log.info(pkgVersion + " installed successfully");
+      log.info(options.name + '@' + options.version + " installed successfully");
       resolve();
     });
   });
